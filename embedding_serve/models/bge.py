95ce69ee0db470e1af9bbe4985ec5e304b4e4a6e
wangwm <wangwm@easypayx.com>
2024-10-19 12:04:00 +0800

    refine

--- embedding_serve/models/bge.py
+++
@@ -1,6 +0,0 @@
-from embedding_serve.engine import EmbeddingModel
-
-
-class BgeM3(EmbeddingModel):
-    def text_sparse_embed(self, batch_sentences: list[str]) -> list[dict[int, float]]:
-        pass

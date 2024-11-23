04147cf4d86dd59e5bc3bad55958c4cc25cb35b9
weiminw <weiminw80105@gmail.com>
2024-11-23 15:23:29 +0800

    update project structure

--- src/bixi/embeddings/__init__.py
+++ src/bixi/embeddings/__init__.py
@@ -1,5 +1,7 @@
 
-from bixi import EmbeddingRequest, DenseEmbeddingData, EmbeddingUsage, \
-    SparseEmbeddingData, EmbeddingResponse
+
 
 __all__ = ["EmbeddingRequest", "DenseEmbeddingData", "EmbeddingUsage", "EmbeddingResponse", "SparseEmbeddingData"]
+
+from bixi.embeddings.protocol import EmbeddingRequest, DenseEmbeddingData, EmbeddingUsage, EmbeddingResponse, \
+    SparseEmbeddingData
--- src/bixi/embeddings/settings/__init__.py
+++ src/bixi/embeddings/settings/__init__.py
@@ -1,3 +1,3 @@
 __all__ = ['configure_logging', 'get_logging_configuration']
 
-from bixi import configure_logging, get_logging_configuration
+from bixi.embeddings.settings.logging_setting import configure_logging, get_logging_configuration
